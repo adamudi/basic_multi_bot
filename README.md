94ab31adf1042408c7ae22a460279bf4d8fd7571
Tom Alexander <craftkiller@gmail.com>
2014-12-22 19:43:24 -0500

    hello world to start file tree

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 basic_multi_bot
 ===============
 
-C++ implementation of a basic irc+xmpp chat bot
+C++ implementation of a basic irc+xmpp chat bot. The goal of this project is to implement from-scratch anything thats practical to implement, as a learning exercise. It is licensed under the ISC license so feel free to use this is any of your projects.
+
+Building
+---------
+Install premake4 and either clang or gcc >= 4.9
+```
+premake4 gmake
+make
+```
---
+++ premake4.lua
@@ -0,0 +1,26 @@
+solution("basic_multi_bot")
+
+configurations({"Debug", "Release"})
+flags({"NoRTTI"})
+
+configuration("Release")
+buildoptions({"-O3"})
+linkoptions({"-O4"}) -- Link time optimization
+flags({"StaticRuntime"})
+
+configuration("Debug")
+flags({"Symbols"})
+
+configuration({})
+
+project("bmbottest")
+kind("ConsoleApp")
+language("C++")
+buildoptions({"-std=c++11"})
+linkoptions({"-pthread"})
+links({"ssl", "crypto"})
+includedirs({"src"})
+libdirs({"/usr/local/lib"})
+files({"src/main.cpp"
+})
+
