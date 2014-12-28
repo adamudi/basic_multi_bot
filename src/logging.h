165c8eb400377b9b4158616de6b7da99b35b1cef
Tom Alexander <craftkiller@gmail.com>
2014-12-27 22:20:52 -0500

    added C++ streams to logging

--- src/logging.h
+++ src/logging.h
@@ -20,9 +20,11 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <iostream>
 #include <cstring>
 
-#define STANDARD (1 << 0)
-#define SOCKET (1 << 1)
-#define OTHER (1 << 2)
+#define MASK_SHIFT(offset) (1 << offset)
+
+#define STANDARD MASK_SHIFT(0)
+#define SOCKET MASK_SHIFT(1)
+#define OTHER MASK_SHIFT(2)
 
 #ifndef LOG_MASK // If LOG_MASK isn't defined, dont log anything but standard
 #define LOG_MASK (STANDARD)
@@ -63,4 +65,32 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * log_all(OTHER | SOCKET, "log if both OTHER or SOCKET are true");
  */
 #define elog_all(filter, cformat, ...) if (((filter) & (LOG_MASK)) == (filter)) fprintf(stderr, "LOG %s:%u: " cformat "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+/**
+ * Print a log message prefixed with "LOG <file>:<lineno>: " to
+ * stdout. It will only print if any of the log types defined by the
+ * first parameter are set to true in LOG_MASK. Example usage:
+ */
+#define slog(filter, ...) if (((filter) & (LOG_MASK)) != 0) std::cout << "LOG " << __FILE__ << ":" << __LINE__ << ": " << __VA_ARGS__ << "\n"
+
+/**
+ * Print a log message prefixed with "LOG <file>:<lineno>: " to
+ * stdout. It will only print if all of the log types defined by the
+ * first parameter are set to true in LOG_MASK. Example usage:
+ */
+#define slog_all(filter, ...) if (((filter) & (LOG_MASK)) == (filter)) std::cout << "LOG " << __FILE__ << ":" << __LINE__ << ": " << __VA_ARGS__ << "\n"
+
+/**
+ * Print a log message prefixed with "LOG <file>:<lineno>: " to
+ * stderr. It will only print if any of the log types defined by the
+ * first parameter are set to true in LOG_MASK. Example usage:
+ */
+#define selog(filter, ...) if (((filter) & (LOG_MASK)) != 0) std::cerr << "LOG " << __FILE__ << ":" << __LINE__ << ": " << __VA_ARGS__ << "\n"
+
+/**
+ * Print a log message prefixed with "LOG <file>:<lineno>: " to
+ * stderr. It will only print if all of the log types defined by the
+ * first parameter are set to true in LOG_MASK. Example usage:
+ */
+#define selog_all(filter, ...) if (((filter) & (LOG_MASK)) == (filter)) std::cerr << "LOG " << __FILE__ << ":" << __LINE__ << ": " << __VA_ARGS__ << "\n"
 
