95ea0c89aebb239a32cfc54a2fdf87ea04ee589e
Tom Alexander <craftkiller@gmail.com>
2014-12-29 00:47:41 -0500

    reduced interval to 100

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 int main(int argc, char** argv)
 {
     try {
-        // std::cout << http_client::get("http://fizz.buzz/");
         connection_manager conn_man;
         //conn_man.take_connection(std::unique_ptr<irc_client>(new irc_client("Test_Moboto", {"#mob", "#mobile", "#rna-dev", "#fancy", "#sitex-dev", "#vr-dev", "#bldev", "#cdsdev", "#tr-dev"}, "irc.tripadvisor.com", "6667")));
         std::unique_ptr<irc_client> irc(new irc_client("Test_Moboto", {"#mob"}, "irc.tripadvisor.com", "6667"));
@@ -20,7 +19,7 @@ int main(int argc, char** argv)
         conn_man.take_connection(std::move(irc));
         while (conn_man.tick())
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     } catch (const ssl_socket_exception & e) {
         std::cerr << e.to_string() << "\n";
