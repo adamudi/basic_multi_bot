b67d4bddd9a6134aae5378deedb1e311410fff75
Tom Alexander <craftkiller@gmail.com>
2014-12-31 20:07:07 -0500

    restricting echo delegate to rooms

--- src/echo_delegate.cpp
+++ src/echo_delegate.cpp
@@ -4,10 +4,27 @@
 #include "echo_delegate.h"
 #include "logging.h"
 
+namespace
+{
+    bool match_protocol_and_room(const address & needle, const std::vector<address> & haystack)
+    {
+        for (const address & addr : haystack)
+        {
+            if (addr.protocol == needle.protocol && addr.host == needle.host)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 std::vector<message> echo_delegate::accept_message(const message & from, const std::string & current_nick)
 {
     std::vector<message> result;
-    if (from.addr.username == current_nick) return result;
+    if (from.addr.username == current_nick || !match_protocol_and_room(from.addr, get_rooms()))
+        return result;
+
     address out_address = {from.addr.protocol, from.addr.host, "", from.addr.room};
     message out_message = {out_address, from.body, from.raw};
     result.push_back(out_message);
