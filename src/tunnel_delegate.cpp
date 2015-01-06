d2648884876264af5252bd90269fbf2740a1746b
Tom Alexander <craftkiller@gmail.com>
2015-01-05 21:40:04 -0500

    added "!priv" and removed name spoofing

--- src/tunnel_delegate.cpp
+++ src/tunnel_delegate.cpp
@@ -14,7 +14,9 @@ namespace
     };
 
     tunnel_pair tunnel_pairs[] = {
-        {{"irc", "irc.tripadvisor.com", "", "#mob"}, {"xmpp", "hipchat.tripadvisorhipchat.com", "", "1_bot_testing"}}
+        {{"irc", "irc.tripadvisor.com", "", "#mob"}, {"xmpp", "hipchat.tripadvisorhipchat.com", "", "1_bot_testing"}},
+        {{"irc", "irc.tripadvisor.com", "", "#tr-dev"}, {"xmpp", "hipchat.tripadvisorhipchat.com", "", "1_#trdev"}},
+        {{"irc", "irc.tripadvisor.com", "", "#mobile"}, {"xmpp", "hipchat.tripadvisorhipchat.com", "", "1_mobile_web"}},
     };
 
     bool match_protocol_and_room(const address & needle, const std::vector<address> & haystack)
@@ -51,36 +53,19 @@ namespace
 
         return dests;
     }
-
-    std::string make_irc_username(const std::string & name)
-    {
-        std::string ret(name);
-        ret.erase(std::remove(ret.begin(), ret.end(), ' '), ret.end());
-        return ret + "_mirror";
-    }
 }
 
 std::vector<message> tunnel_delegate::accept_message(const message & from, const std::string & current_nick)
 {
     std::vector<message> result;
-    if (from.addr.username == current_nick || !match_protocol_and_room(from.addr, get_rooms()))
+    if (from.addr.username == current_nick || !match_protocol_and_room(from.addr, get_rooms()) || from.body.find("!priv") != std::string::npos)
         return result;
 
     for (const address & addr : get_destinations(from.addr))
     {
         address out_address = {addr.protocol, addr.host, from.addr.username, addr.room};
-        if (addr.protocol == "irc")
-        {
-            message change_nick = {out_address, "NICK :" + make_irc_username(from.addr.username) + "\n", true};
-            message out_message = {out_address, from.body, from.raw};
-            message change_back = {out_address, "NICK :" IRC_NAME "\n", true};
-            result.push_back(change_nick);
-            result.push_back(out_message);
-            result.push_back(change_back);
-        } else {
-            message out_message = {out_address, from.addr.username + ": " + from.body, from.raw};
-            result.push_back(out_message);
-        }
+        message out_message = {out_address, from.addr.username + ": " + from.body, from.raw};
+        result.push_back(out_message);
     }
     
     return result;
