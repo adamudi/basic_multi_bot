d729749b0ed86fb2232993dae0d3d699683ebcb9
Tom Alexander <craftkiller@gmail.com>
2015-02-26 06:40:04 -0500

    added equality to address

--- src/address.h
+++ src/address.h
@@ -5,6 +5,7 @@
  */
 #pragma once
 #include <string>
+#include <tuple>
 
 struct address
 {
@@ -13,3 +14,8 @@ struct address
     const std::string username;
     const std::string room;
 };
+
+inline bool match_except_username(const address & a, const address & b)
+{
+    return std::tie(a.protocol, a.host, a.room) == std::tie(b.protocol, b.host, b.room);
+}
